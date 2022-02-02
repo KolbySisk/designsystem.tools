d65d7d741cc9693ebc1814efdbba631b2c701017
Kolby Sisk <kolby.sisk@gmail.com>
2022-02-02 13:19:02 -0600

    feat: add seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
 
 import { Features } from '~/components/features'
 import { Footer } from '~/components/footer'
@@ -7,12 +8,30 @@ import { Intro } from '~/components/intro'
 
 const Home: NextPage = () => {
   return (
-    <main>
-      <Header />
-      <Intro />
-      <Features />
-      <Footer />
-    </main>
+    <>
+      <NextSeo
+        title='Design Systems Demystified'
+        description='This website aims to explain what design systems are, the advantages of building one, and the tools involved.'
+        openGraph={{
+          url: 'https://designsystem.tools/',
+          title: 'designsystems.tools',
+          description:
+            'This website aims to explain what design systems are, the advantages of building one, and the tools involved.',
+          site_name: 'designsystems.tools',
+        }}
+        twitter={{
+          handle: '@kolbysisk',
+          site: '@kolbysisk',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <main>
+        <Header />
+        <Intro />
+        <Features />
+        <Footer />
+      </main>
+    </>
   )
 }
 
