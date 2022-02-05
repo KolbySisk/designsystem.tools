af55b7d5d484670014466950a82d0d442a458e38
Kolby Sisk <kolby.sisk@gmail.com>
2022-02-05 11:44:36 -0600

    feat: add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -17,6 +18,16 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@400;500;700&display=swap'
             rel='stylesheet'
           />
+          <Script src='https://www.googletagmanager.com/gtag/js?id=G-013R48ZCGQ' strategy='afterInteractive' />
+          <Script id='google-analytics' strategy='afterInteractive'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-013R48ZCGQ');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
